3a629c8a70d005912b796a9e013e047980eb1f16
idkgimah <f25687883@gmail.com>
2025-07-20 01:04:48 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/nlWMuvroN3/0", true))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/azbalalaff/loading-screen/refs/heads/main/loader.lua"))()
